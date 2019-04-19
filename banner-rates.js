c99063ab9ee512fbf26fbf5806cb553904c0a4ab
sorasei94 <sorasei94@gmail.com>
2019-04-19 12:18:06 +0700

    Create banner-rates.js

---
+++ banner-rates.js
@@ -0,0 +1,42 @@
+var Rates = {
+    NormalBanner: [
+        [99, "5", "hero"],
+        [0.1, "4", "hero"],
+        [0.1, "3", "hero"],
+        [0.6, "5", "artifact"],
+        [0.1, "4", "artifact"],
+        [0.1, "3", "artifact"]
+    ],
+    RateUpBanner: [
+        [0.8, "New", "hero"],
+        [0.45, "5", "hero"],
+        [4.5, "4", "hero"],
+        [41, "3", "hero"],
+        [1.05, "New", "artifact"],
+        [0.7, 5, "artifact"],
+        [6.5, 4, "artifact"],
+        [45, 3, "artifact"]
+    ],
+    LimitedBanner: [
+        [1, "New", "hero"],
+        [4.5, "4", "hero"],
+        [41.25, "3", "hero"],
+        [1.75, "5", "artifact"],
+        [6.5, "4", "artifact"],
+        [45, "3", "artifact"]
+    ],
+    MoonlightBanner: [
+        [99, "5", "hero"],
+        [0.5, "4", "hero"],
+        [0.5, "3", "hero"],
+    ],
+    DoubleBanner: [
+        [0.8, "New", "hero"],
+        [0.45, "5", "hero"],
+        [4.5, "4", "hero"],
+        [41, "3", "hero"],
+        [1.75, 5, "artifact"],
+        [6.5, 4, "artifact"],
+        [45, 3, "artifact"]
+    ]
+}
